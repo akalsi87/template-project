0268476c00561b75465d86032173401c24ff80e2
Aaditya Kalsi <akalsi.dev@gmail.com>
2017-04-06 00:38:49 -0400

    Make tests/install/test.sh check for errors

--- tests/install/test.sh
+++ tests/install/test.sh
@@ -4,6 +4,8 @@ currdir=`dirname $exec`
 parent=`dirname $currdir`
 root=`dirname $parent`
 
+set -e
+
 cmake -H. -B$currdir/_build -DCMAKE_INSTALL_PREFIX=$root/_install -DTESTS_DIR=$root/tests
 env VERBOSE=1 cmake --build $currdir/_build --target tests_run > $currdir/testLog.txt
 rm -fr $currdir/_build
