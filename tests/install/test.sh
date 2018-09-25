4a382c77c0d7f73ffa521369331f1a92a1e16d43
Aaditya Kalsi <akalsi.dev@gmail.com>
2018-09-25 10:51:49 -0700

    Fix test on install test on Windows

--- tests/install/test.sh
+++ tests/install/test.sh
@@ -8,8 +8,20 @@ root=`dirname $parent`
 
 set -e
 
-cmake -H. -B$currdir/_build -DCMAKE_INSTALL_PREFIX=$root/_install -DTESTS_DIR=$root/tests
-env VERBOSE=1 cmake --build $currdir/_build --target tests_run > $currdir/testLog.txt
+if [ "$GENERATOR" != "" ]; then
+    gen_arg=-G"${GENERATOR}"
+else
+    gen_arg=
+fi
+
+if [ "$CMAKE_BUILD_TYPE" == "" ]; then
+    export CMAKE_BUILD_TYPE=Debug
+fi
+
+bld_arg="-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
+
+cmake -H. -B$currdir/_build -DCMAKE_INSTALL_PREFIX=$root/_install -DTESTS_DIR=$root/tests "$gen_arg" $bld_arg
+env VERBOSE=1 cmake --build $currdir/_build --target tests_run --config $CMAKE_BUILD_TYPE > $currdir/testLog.txt
 rm -fr $currdir/_build
 
 echo '----------------------------------------------------------'
